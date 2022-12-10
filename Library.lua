80991b27dd5684939224349a893f556e0856c926
Incognito <75094867+lncoognito@users.noreply.github.com>
2022-12-10 21:17:52 +0300

    Create Library.lua

--- Library.lua
+++ Library.lua
@@ -954,7 +954,11 @@ do
                     local Key;
 
                     if Input.UserInputType == Enum.UserInputType.Keyboard then
-                        Key = Input.KeyCode.Name;
+                        if Input.KeyCode.Name == "Delete" then
+                            Key = "Unknown"
+                        else
+                            Key = Input.KeyCode.Name;
+                        end
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton1 then
                         Key = 'MB1';
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton2 then
