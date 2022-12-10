a7cf4b067fb1baf677cda94a3230c621de54e243
Incognito <75094867+lncoognito@users.noreply.github.com>
2022-12-10 20:57:37 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -851,7 +851,7 @@ do
         end;
 
         function KeyPicker:Update()
-            if Info.NoUI then
+            if Info.NoUI and KeyPicker.Value == "Unknown" then
                 return;
             end;
 
