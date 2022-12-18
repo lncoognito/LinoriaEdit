592c821c18de7e8a13840cc1f43140b0d6d4af19
Incognito <75094867+lncoognito@users.noreply.github.com>
2022-12-18 16:24:17 +0300

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -4,148 +4,158 @@ local SaveManager       = {}
 SaveManager.Folder      = "LinoriaLibSave"
 SaveManager.Ignore      = {}
 SaveManager.Parser      = {
-    Toggle = {
-        Save = function(Index, Object) 
-            return { type = "Toggle", idx = Index, value = Object.Value } 
-        end,
+	Toggle = {
+		Save = function(Index, Object) 
+			return { type = "Toggle", idx = Index, value = Object.Value } 
+		end,
 
-        Load = function(Index, Data)
-            if Toggles[Index] then 
-                Toggles[Index]:SetValue(Data.value)
-            end
-        end
-    },
+		Load = function(Index, Data)
+			if Toggles[Index] then 
+				Toggles[Index]:SetValue(Data.value)
+			end
+		end
+	},
 
-    Slider = {
-        Save = function(Index, Object)
-            return { type = "Slider", idx = Index, value = tostring(Object.Value) }
-        end,
+	Slider = {
+		Save = function(Index, Object)
+			return { type = "Slider", idx = Index, value = tostring(Object.Value) }
+		end,
 
-        Load = function(Index, Data)
-            if Options[Index] then 
-                Options[Index]:SetValue(Data.value)
-            end
-        end
-    },
+		Load = function(Index, Data)
+			if Options[Index] then 
+				Options[Index]:SetValue(Data.value)
+			end
+		end
+	},
 
-    Dropdown = {
-        Save = function(Index, Object)
-            return { type = "Dropdown", idx = Index, value = Object.Value, mutli = Object.Multi }
-        end,
+	Dropdown = {
+		Save = function(Index, Object)
+			return { type = "Dropdown", idx = Index, value = Object.Value, mutli = Object.Multi }
+		end,
 
-        Load = function(Index, Data)
-            if Options[Index] then 
-                Options[Index]:SetValue(Data.value)
-            end
-        end
-    },
+		Load = function(Index, Data)
+			if Options[Index] then 
+				Options[Index]:SetValue(Data.value)
+			end
+		end
+	},
+	ColorPicker = {
+		Save = function(Index, Object)
+			return { type = "ColorPicker", idx = Index, value = Object.Value:ToHex() }
+		end,
 
-    ColorPicker = {
-        Save = function(Index, Object)
-            return { type = "ColorPicker", idx = Index, value = Object.Value:ToHex() }
-        end,
+		Load = function(Index, Data)
+			if Options[Index] then 
+				Options[Index]:SetValueRGB(Color3.fromHex(Data.value))
+			end
+		end
+	},
 
-        Load = function(Index, Data)
-            if Options[Index] then 
-                Options[Index]:SetValueRGB(Color3.fromHex(Data.value))
-            end
-        end
-    },
+	KeyPicker = {
+		Save = function(Index, Object)
+			return { type = "KeyPicker", idx = Index, mode = Object.Mode, key = Object.Value }
+		end,
 
-    KeyPicker = {
-        Save = function(Index, Object)
-            return { type = "KeyPicker", idx = Index, mode = Object.Mode, key = Object.Value }
-        end,
+		Load = function(Index, Data)
+			if Options[Index] then 
+				Options[Index]:SetValue({ Data.key, Data.mode })
+			end
+		end
+	},
 
-        Load = function(Index, Data)
-            if Options[Index] then 
-                Options[Index]:SetValue({ Data.key, Data.mode })
-            end
-        end
-    }
+	Input = {
+		Save = function(Index, Object)
+			return { type = "Input", idx = Index, text = Object.Value }
+		end,
+
+		Load = function(Index, Data)
+			if Options[Index] and type(Data.text) == "string" then
+				Options[Index]:SetValue(Data.text)
+			end
+		end
+	},
 }
 
 function SaveManager:SetIgnoreIndexes(List)
-    for _, Key in next, List do
-        self.Ignore[Key] = true
-    end
+	for i, Key in next, List do
+		self.Ignore[Key] = true
+	end
 end
 
 function SaveManager:SetFolder(Folder)
-    self.Folder = Folder
-
-    self:BuildFolderTree()
+	self.Folder = Folder
+	self:BuildFolderTree()
 end
 
 function SaveManager:Save(Name)
-    local FullPath = self.Folder .. "/Settings/" .. Name .. ".json"
+	local FullPath 	= self.Folder .. "/Settings/" .. Name .. ".json"
 
-    local Data = {
-        Objects = {}
-    }
+	local Data = {
+		Objects = {}
+	}
 
-    for Index, Toggle in next, Toggles do
-        if self.Ignore[Index] then continue end
+	for Index, Toggle in next, Toggles do
+		if self.Ignore[Index] then continue end
 
-        table.insert(Data.Objects, self.Parser[Toggle.Type].Save(Index, Toggle))
-    end
+		table.insert(Data.Objects, self.Parser[Toggle.Type].Save(Index, Toggle))
+	end
 
-    for Index, Option in next, Options do
-        if not self.Parser[Option.Type] then continue end
-        if self.Ignore[Index] then continue end
+	for Index, Option in next, Options do
+		if not self.Parser[Option.Type] then continue end
+		if self.Ignore[Index] then continue end
 
-        table.insert(Data.Objects, self.Parser[Option.Type].Save(Index, Option))
-    end	
+		table.insert(Data.Objects, self.Parser[Option.Type].Save(Index, Option))
+	end	
 
-    local Success, Encoded = pcall(HttpService.JSONEncode, HttpService, Data)
+	local Success, Encoded = pcall(HttpService.JSONEncode, HttpService, Data)
 
-    if not Success then
-        return false, "Failed to encode."
-    end
+	if not Success then
+		return false, "Failed to encode data."
+	end
 
-    writefile(FullPath, Encoded)
+	writefile(FullPath, Encoded)
 
-    return true
+	return true
 end
 
 function SaveManager:Load(Name)
-    local File = self.Folder .. "/Settings/" .. Name .. ".json"
+	local File 	= self.Folder .. "/Settings/" .. Name .. ".json"
+	if not isfile(File) then return false, "Invalid file." end
 
-    if not isfile(File) then return false, "Invalid file." end
+	local Success, Encoded = pcall(HttpService.JSONEncode, HttpService, readfile(File))
 
-    local Success, Decoded = pcall(HttpService.JSONDecode, HttpService, readfile(File))
-    if not Success then return false, "Decode error." end
+	if not Success then return false, "Decode error." end
 
-    for _, Option in next, Decoded.Objects do
-        if self.Parser[Option.type] then
-            self.Parser[Option.type].Load(Option.idx, Option)
-        end
-    end
+	for Index, Option in next, Decoded.Objects do
+		if self.Parser[Option.type] then
+			self.Parser[Option.type].Load(Option.idx, Option)
+		end
+	end
 
-    return true
+	return true
 end
 
 function SaveManager:IgnoreThemeSettings()
-    self:SetIgnoreIndexes({ 
-        "BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor",
-        "ThemeManager_ThemeList", "ThemeManager_CustomThemeList", "ThemeManager_CustomThemeName",
-    })
+	self:SetIgnoreIndexes({ 
+		"BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor",
+		"ThemeManager_ThemeList", "ThemeManager_CustomThemeList", "ThemeManager_CustomThemeName",
+	})
 end
 
 function SaveManager:BuildFolderTree()
-    local Paths = {
-        self.Folder,
-        self.Folder .. "/Themes",
-        self.Folder .. "/Settings"
-    }
+	local Paths = {
+		self.Folder,
+		self.Folder .. "/Themes",
+		self.Folder .. "/Settings"
+	}
 
-    for i = 1, #Paths do
-        local String = Paths[i]
+	for i = 1, #Paths do
+		local String	= Paths[i]
 
-        if not isfolder(String) then
-            makefolder(String)
-        end
-    end
+		if not isfolder(String) then
+			makefolder(String)
+		end
+	end
 end
 
 function SaveManager:RefreshConfigList()
