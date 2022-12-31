c9ad2c83abdb3ba981ce7b207cf7ca3fd775f47f
Incognito <75094867+lncoognito@users.noreply.github.com>
2022-12-31 11:25:39 +0300

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,3 @@
+getgenv().Library       = loadstring(game:HttpGet("https://raw.githubusercontent.com/lncoognito/LinoriaEdit/main/Library.lua"))()
+getgenv().SaveManager   = loadstring(game:HttpGet("https://raw.githubusercontent.com/lncoognito/LinoriaEdit/main/SaveManager.lua"))()
+getgenv().ThemeManager  = loadstring(game:HttpGet("https://raw.githubusercontent.com/lncoognito/LinoriaEdit/main/ThemeManager.lua"))()
